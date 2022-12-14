89573f853dd4b22942e3ae61c97d4f38f2470ce1
Alex1986nder <99564342+Alex1986nder@users.noreply.github.com>
2022-12-14 23:33:48 +0300

    правки

--- src/api/serverLogin.js
+++ src/api/serverLogin.js
@@ -1,5 +1,13 @@
 export const serverLogin = async (email, password) => {
-  return fetch(
-    `https://loft-taxi.glitch.me/auth?username=${email}&password=${password}`
-  ).then(res => res.json()).then(data => data.success);
+  // return fetch(
+  //   `https://loft-taxi.glitch.me/auth?username=${email}&password=${password}`
+  // ).then(res => res.json()).then(data => data.success);
+  const ops = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  };
+
+  return fetch(`https://loft-taxi.glitch.me/auth`, ops)
+    .then((response) => response.json())
 };
