922be0597422e260a2b59ebccaac1f9deeddb524
Alex1986nder <99564342+Alex1986nder@users.noreply.github.com>
2022-12-24 01:33:53 +0300

    сделал окно- заказ размещен

--- src/components/Map/MapOverlay.jsx
+++ src/components/Map/MapOverlay.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
-import {  useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { coordinatesSelector } from "../../reducers/route";
-
+import { resetCoordinates } from "../../action";
 
 export const MapOverlay = () => {
-
+  const [show, setShow] = useState(false);
   const coordinates = useSelector(coordinatesSelector);
-
-  if (coordinates.length) {
+  const dispatch = useDispatch()
+  const reset = () => {
+    dispatch(resetCoordinates());
+  }
+  const onclick = (e) => {
+    e.preventDefault();
+      setShow(show === true);
+      reset();
+  }
+  if (coordinates.length && show === false) {
     return (
       <div className="map__message">
         <h1 className="map__message-title">Заказ размещен</h1>
@@ -17,7 +25,13 @@ export const MapOverlay = () => {
           Ваше такси уже едет к вам. Прибудет приблизительно через 10 минут.
         </p>
         <div className="profile__button">
-          <button type="submit" className="btn btn--profile" onChange={() =>{}}>Сделать новый заказ</button>
+          <button
+            type="submit"
+            className="btn btn--profile"
+            onClick={onclick}
+          >
+            Сделать новый заказ
+          </button>
         </div>
       </div>
     );
