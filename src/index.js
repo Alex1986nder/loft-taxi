43fb34c4ce696383d34856a2e6a2758e6bae5732
Alex1986nder <99564342+Alex1986nder@users.noreply.github.com>
2025-05-05 20:36:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename='https://github.com/Alex1986nder/loft-taxi'>
+  <BrowserRouter>
     <Provider store={store}>
         <App />
     </Provider>
